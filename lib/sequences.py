bfea0487b29b1a53a03c8e974cfd5d2e9ae27f86
Andy Patari <andy.patari@gmail.com>
2023-09-13 09:32:12 -0400

    passing tests

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    c = 0
+    prev = 1
+    temp = 0
+    res = []
+    for i in range(length):
+            res.append(c)
+            temp = c
+            c += prev
+            prev = temp
+
+    print(res)
